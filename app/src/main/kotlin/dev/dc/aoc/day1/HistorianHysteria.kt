79b055416b584ad0089e80480b2e0f95068b2862
Dean Cowley <dean.w.cowley@gmail.com>
2024-12-01 08:55:46 +0000

    D01-P2: Similarity score between location ID lists

--- app/src/main/kotlin/dev/dc/aoc/day1/HistorianHysteria.kt
+++ app/src/main/kotlin/dev/dc/aoc/day1/HistorianHysteria.kt
@@ -3,30 +3,31 @@ package dev.dc.aoc.day1
 import java.io.FileNotFoundException
 import kotlin.math.absoluteValue
 
-
 fun main() {
     val inputFileName = "location_ids.csv"
     val inputFile = ClassLoader.getSystemResourceAsStream(inputFileName) ?: throw FileNotFoundException(inputFileName)
     val inputLines = inputFile.bufferedReader().readLines()
 
     totalDistance(inputLines)
+    similarityScore(inputLines)
 }
 
 fun totalDistance(inputLines: List<String>) {
     val locationIds1 = mutableListOf<Int>()
     val locationIds2 = mutableListOf<Int>()
     inputLines.forEach {
-        locationIds1.sortedInsert(it.substringBefore(',').toInt())
-        locationIds2.sortedInsert(it.substringAfter(',').toInt())
+        val parts = it.split(',')
+        locationIds1.sortedInsert(parts[0].toInt())
+        locationIds2.sortedInsert(parts[1].toInt())
     }
 
     val totalDistance = locationIds1
         .zip(locationIds2)
-        .fold(0) { acc, pair ->
+        .fold(initial = 0) { acc, pair ->
             acc + (pair.first - pair.second).absoluteValue
         }
 
-    println("The total distance between both lists is: $totalDistance")
+    println("Total distance: $totalDistance")
 }
 
 fun MutableList<Int>.sortedInsert(element: Int) {
@@ -35,4 +36,23 @@ fun MutableList<Int>.sortedInsert(element: Int) {
             if (it >= 0) it else -it - 1
         }
     add(insertIndex, element)
+}
+
+fun similarityScore(inputLines: List<String>) {
+    val locationIds = mutableListOf<Int>()
+    val frequencies = mutableMapOf<Int, Int>()
+
+    inputLines.forEach {
+        val parts = it.split(',')
+        locationIds += parts[0].toInt()
+
+        frequencies.compute(parts[1].toInt()) { _, v ->
+            (v ?: 0) + 1
+        }
+    }
+
+    val similarityScore = locationIds.fold(initial = 0) { acc, i ->
+        acc + (i * frequencies.getOrDefault(i, 0))
+    }
+    println("Similarity score: $similarityScore")
 }
